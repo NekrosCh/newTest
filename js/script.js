49bf6993349750ccfa5554b79c95db9569b49435
Yan <elvhant@gmail.com>
2023-07-16 16:43:08 +0300

    Second

--- js/script.js
+++ js/script.js
@@ -63,20 +63,20 @@
 
 console.log(4 + +"6");
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// ++incr;
-// --decr;
+// // ++incr;
+// // --decr;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(5%2);
+// console.log(5%2);
 
-console.log(2 + 2 * 2 === 8);
+// console.log(2 + 2 * 2 != 8);
 
-const isChecked = true,
-      isClose = false;
+// const isChecked = true,
+//       isClose = false;
 
-console.log(isChecked && !isClose);
+// console.log(isChecked && !isClose);
