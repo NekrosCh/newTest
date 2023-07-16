f433a0d232bc86f93cf177e09feee2b93900511a
Yan <elvhant@gmail.com>
2023-07-16 16:39:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+"use strict"; 
+
+// let number = 5;
+// const leftBorderWidth = 1;
+ 
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+
+// };
+
+// obj.a = 11;
+
+// console.log(obj);
+
+// {
+//     let result =50;
+// }
+
+// let number = 4.6;
+// console.log(-4/0); // infiniti
+// console.log("string" * 9); // nan
+
+// const persone = "Alex";
+
+// const bool = true; 
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// // console.log(obj.name)
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[1]);
+
+// alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?" , "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = "toys";
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = "Yan";
+// alert(`Привет, ${user}!`);
+
+console.log(4 + +"6");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 === 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && !isClose);
